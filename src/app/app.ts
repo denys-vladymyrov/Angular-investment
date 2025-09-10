a30ee876b70bfe46347912db0ae5a0038f0c192e
Denys Vladymyrov <link_amd@yahoo.com>
2025-09-10 23:48:52 +0300

    added 'calculateInvestmentResults' method

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,9 @@
-import { Component, signal } from '@angular/core';
+import {Component} from '@angular/core';
 import {Header} from './components/header/header';
 import {UserInput} from './components/user-input/user-input';
+import {InvestmentResults} from './models';
+
+
 
 @Component({
   selector: 'app-root',
@@ -9,5 +12,28 @@ import {UserInput} from './components/user-input/user-input';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('investment');
+  calculateInvestmentResults(data: InvestmentResults) {
+    const {initialInvestment, annualInvestment, expectedReturn, duration} = data;
+    const annualData = [];
+    let investmentValue = initialInvestment;
+
+    for (let i = 0; i < duration; i++) {
+      const year = i + 1;
+      const interestEarnedInYear = investmentValue * (expectedReturn / 100);
+      investmentValue += interestEarnedInYear + annualInvestment;
+      const totalInterest = investmentValue - annualInvestment * year - initialInvestment;
+
+      annualData.push({
+        year: year,
+        interest: interestEarnedInYear,
+        valueEndOfYear: investmentValue,
+        annualInvestment: annualInvestment,
+        totalInterest: totalInterest,
+        totalAmountInvested: initialInvestment + annualInvestment * year,
+      });
+    }
+
+    console.log(annualData);
+    return annualData;
+  }
 }
--- src/app/components/user-input/user-input.ts
+++ src/app/components/user-input/user-input.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, output} from '@angular/core';
+import {FormsModule} from '@angular/forms';
+import {InvestmentResults} from '../../models';
 
 @Component({
   selector: 'app-user-input',
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './user-input.html',
   styleUrl: './user-input.scss'
 })
 export class UserInput {
+  calculate = output<InvestmentResults>()
+  enteredInitialInvestment = '0';
+  enteredAnnualInvestment = '0';
+  enteredExpectedReturn = '5';
+  enteredDuration = '10';
 
+  onSubmit() {
+    this.calculate.emit({
+      initialInvestment: +this.enteredInitialInvestment,
+      duration: +this.enteredDuration,
+      expectedReturn: +this.enteredExpectedReturn,
+      annualInvestment: +this.enteredAnnualInvestment,
+    })
+  }
 }
